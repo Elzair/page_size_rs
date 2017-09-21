e811e1ac81bcc0abee73809f29f227b570ef16cd
Philip Woods <elzairthesorcerer@gmail.com>
2017-09-21 16:33:17 -0400

    Changed fn name to get and wrote documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,20 @@
 #![no_std]
+//! This crate provides an easy, fast, cross-platform way to retrieve the
+//! memory page size of the current system. Modern hardware and software tend
+//! to load data into RAM (and transfer data from RAM to disk) in discrete
+//! chunk called pages. This crate provides a helper method to retrieve the size
+//! in bytes of these pages. Since the page size *should not* change during
+//! execution, this crate will cache the result after it has been called once.
+//! To make this crate useful for writing memory allocators, it does not require
+//! (but can use) the Rust standard library. 
+//!
+//! # Example
+//!
+#![cfg_attr(not(feature = "no-std"), doc = " ``` ")]
+#![cfg_attr(feature = "no-std", doc = " ```no_run ")]
+//! extern crate page_size;
+//! println!("{}", page_size::get());
+//! ```
 
 // `const_fn` is needed for `spin::Once`.
 #![cfg_attr(feature = "no-std", feature(const_fn))]
@@ -13,28 +29,37 @@ extern crate std;
 #[cfg(not(feature = "no-std"))]
 use std::sync::{Once, ONCE_INIT};
 
-pub fn get_size() -> usize {
-    get_size_helper()
+/// This function retrieves the system's memory page size.
+///
+/// # Example
+///
+#[cfg_attr(not(feature = "no-std"), doc = " ``` ")]
+#[cfg_attr(feature = "no-std", doc = " ```no_run ")]
+/// extern crate page_size;
+/// println!("{}", page_size::get());
+/// ```
+pub fn get() -> usize {
+    get_helper()
 }
 
 // Unix Section
 
 #[cfg(all(unix, feature = "no-std"))]
 #[inline]
-fn get_size_helper() -> usize {
+fn get_helper() -> usize {
     static INIT: Once<usize> = Once::new();
     
-    *INIT.call_once(unix::get_size_unix)
+    *INIT.call_once(unix::get_unix)
 }
 
 #[cfg(all(unix, not(feature = "no-std")))]
 #[inline]
-fn get_size_helper() -> usize {
+fn get_helper() -> usize {
     static INIT: Once = ONCE_INIT;
     static mut PAGE_SIZE: usize = 0;
 
     unsafe {
-        INIT.call_once(|| PAGE_SIZE = unix::get_size_unix());
+        INIT.call_once(|| PAGE_SIZE = unix::get_unix());
         PAGE_SIZE
     }
 }
@@ -47,7 +72,7 @@ mod unix {
     use super::*;
 
     #[inline]
-    pub fn get_size_unix() -> usize {
+    pub fn get_unix() -> usize {
         unsafe {
             libc::sysconf(libc::_SC_PAGESIZE) as usize
         }
@@ -61,20 +86,20 @@ extern crate winapi;
 
 #[cfg(all(windows, feature = "no-std"))]
 #[inline]
-fn get_size_helper() -> usize {
+fn get_helper() -> usize {
     static INIT: Once<usize> = Once::new();
     
-    *INIT.call_once(windows::get_size_windows)
+    *INIT.call_once(windows::get_windows)
 }
 
 #[cfg(all(windows, not(feature = "no-std")))]
 #[inline]
-fn get_size_helper() -> usize {
+fn get_helper() -> usize {
     static INIT: Once = ONCE_INIT;
     static mut PAGE_SIZE: usize = 0;
 
     unsafe {
-        INIT.call_once(|| PAGE_SIZE = windows::get_size_windows());
+        INIT.call_once(|| PAGE_SIZE = windows::get_windows());
         PAGE_SIZE
     }
 }
@@ -84,7 +109,7 @@ mod windows {
     use super::*;
 
     #[inline]
-    pub fn get_size_windows() -> usize {
+    pub fn get_windows() -> usize {
         unsafe {
             let mut info: winapi::SYSTEM_INFO = mem::zeroed();
             winapi::kernel32::GetSystemInfo(&mut info);
@@ -99,8 +124,8 @@ mod tests {
     use super::*;
     
     #[test]
-    fn test_get_size() {
+    fn test_get() {
         #[allow(unused_variables)]
-        let page_size = get_size();
+        let page_size = get();
     }
 }
